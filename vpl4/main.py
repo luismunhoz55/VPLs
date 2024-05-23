7c701ba9d6dfe92be08dd4d199625d60b1f68210
Luis Munhoz <munhozluisfernando@gmail.com>
2024-05-23 08:29:52 -0300

    Add files via upload

---
+++ vpl4/main.py
@@ -0,0 +1,8 @@
+from Janela import Janela
+
+##################################################
+
+## Questão 14: (Aloque um objeto do tipo Janela e mostre-o na tela)
+Jan1=Janela("Minha Calculadora")
+Jan1.mainloop()
+##################################################
